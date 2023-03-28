1290483800ee9a9c07661af6978de47666f737d1
tung.huynh <tung.huynh@setel.com>
2023-03-29 00:20:28 +0700

    Chore: add comment to function

--- cypress/support/pageObjects/SwagLabsCheckoutCompletePage.js
+++ cypress/support/pageObjects/SwagLabsCheckoutCompletePage.js
@@ -1,9 +1,16 @@
 export default class SwagLabsCheckoutCompletePage {
+	/**
+	 * This function validates that the current page URL is the same as the URL defined in the environment
+	 * variable `sauce_demo_url` and appends `checkout-complete.html` to the end of it
+	 */
 	validatePageUrl() {
 		cy.url().should('deep.equal', `${Cypress.env('sauce_demo_url')}checkout-complete.html`);
 		cy.screenshot();
 	}
 
+	/**
+	 * This function validates that the page title is visible and has the correct text
+	 */
 	validatePageTitle() {
 		cy.get('.header_secondary_container').should('be.visible').and('have.css', 'display', 'flex');
 		cy.get('.header_secondary_container span').then(($el) => {
@@ -12,6 +19,12 @@ export default class SwagLabsCheckoutCompletePage {
 		});
 	}
 
+	/**
+	 * This function validates the checkout complete container on the checkout complete page
+	 * @param completeHeader - The text that should be displayed in the header of the checkout complete
+	 * container.
+	 * @param successMsg - The message that is displayed on the checkout complete page.
+	 */
 	validateCheckoutCompleteContainer(completeHeader, successMsg) {
 		cy.get('#checkout_complete_container')
 			.should('be.visible')
